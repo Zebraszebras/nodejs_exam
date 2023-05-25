f71e63b8f921132325cb39e862cf1c76a5e5b7da
Zebraszebras <121254516+Zebraszebras@users.noreply.github.com>
2023-05-25 18:50:06 +0300

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 3 },
-  password: { type: String, required: true, minlength: 15 },
+  password: { type: String, required: true, minlength: 6 },
   email: { type: String, required: true, minlength: 8 },
   _id: { type: String, required: true, minlength: 3 },
-  bought_tickets: [{ type: mongoose.Schema.Types.ObjectId, ref: "Ticket" }],
+  bought_tickets: [{ type: String, ref: "Ticket" }],
   money_balance: { type: Number, required: true },
 });
 
